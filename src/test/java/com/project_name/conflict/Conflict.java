e5b704c7442e9b6338d875f0badc3208ce7e15a3
Owen <owen.constantine@cydeo.com>
2024-01-19 00:58:51 +0300

    conflictAgain

--- src/test/java/com/project_name/conflict/Conflict.java
+++
@@ -1,7 +0,0 @@
-package com.project_name.conflict;
-
-public class Conflict {
-    public static void main(String[] args) {
-        System.out.println("Hello CYDEO");
-    }
-}
---
+++ src/test/java/com/project_name/conflict2/Conflict2.java
@@ -0,0 +1,4 @@
+package com.project_name.conflict2;
+
+public class Conflict2 {
+}
